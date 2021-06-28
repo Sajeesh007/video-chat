b0161818b54704bbbc0c14b11ec441ffd24f88c3
Sajeesh007 <70201622+Sajeesh007@users.noreply.github.com>
2021-06-28 21:02:55 +0530

    uservideo

--- src/components/videoPlayer/UserVideo.jsx
+++ src/components/videoPlayer/UserVideo.jsx
@@ -8,25 +8,24 @@ export default function UserVideo({width,height}) {
   const {userVideo,userMicOn,call,userVideoOn,myVideo} = useContext(SocketContext);
 
 
-  {console.log(userVideoOn)}
-  {console.log(userMicOn)}
+  {console.log('video'+userVideoOn)}
 
   return (
     <div className='user-video-container'>
       {/* <div className="my-video-draggable">
         <video ref={myVideo} playsInline muted autoPlay />
-      
       </div> */}
 
       <div className="profile-info">
           <img src={`https://ui-avatars.com/api/?name=${call.name}&background=0add8c&color=ffffff`} alt="img" />
           <h2>{call.name}</h2>
       </div>
-      {(userVideoOn) ? (
+      {(userVideoOn) && (
       <div className="skelton">
         <img src={`https://ui-avatars.com/api/?name=${call.name}&background=0add8c&color=ffffff`} alt="img" />
-      </div> ) :
-      (<video className='user-video' playsInline {...userMicOn ? {muted : true} : {}}ref={userVideo} autoPlay width={`${width}%`} height={`${height}%`}
+      </div> )}
+       
+      <video className={userVideoOn ? 'user-video-hide' : 'user-video'} playsInline muted={userMicOn} ref={userVideo} autoPlay width={`${width}%`} height={`${height}%`}
         style={{  
           width:`${width}%`,
           height:`${height}%`,
